7e2c3cd26b9eb82e3af09e1b8f086f3d10b8cc59
Andrew Parker <andrew.parker@nrel.gov>
2014-12-15 12:25:13 -0700

    Adds duct routing measure

---
+++ measures/improved_duct_routing/measure.rb
@@ -0,0 +1,121 @@
+# see the URL below for information on how to write OpenStudio measures
+# http://openstudio.nrel.gov/openstudio-measure-writing-guide
+
+# start the measure
+class ImprovedDuctRouting < OpenStudio::Ruleset::ModelUserScript
+
+  # human readable name
+  def name
+    return "Improved Duct Routing"
+  end
+
+  # human readable description
+  def description
+    return ""
+  end
+
+  # human readable description of modeling approach
+  def modeler_description
+    return ""
+  end
+
+  # define the arguments that the user will input
+  def arguments(model)
+    args = OpenStudio::Ruleset::OSArgumentVector.new
+
+    # Make an argument to apply/not apply this measure
+    chs = OpenStudio::StringVector.new
+    chs << "TRUE"
+    chs << "FALSE"
+    apply_measure = OpenStudio::Ruleset::OSArgument::makeChoiceArgument('apply_measure', chs, true)
+    apply_measure.setDisplayName("Apply Measure?")
+    apply_measure.setDefaultValue("TRUE")
+    args << apply_measure 
+    
+    # Make an argument for the percent pressure drop reduction
+    pressure_drop_reduction_pct = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("pressure_drop_reduction_pct",true)
+    pressure_drop_reduction_pct.setDisplayName("Pressure Drop Reduction Percent")
+    pressure_drop_reduction_pct.setUnits("%")
+    pressure_drop_reduction_pct.setDefaultValue(10.0)
+    args << pressure_drop_reduction_pct
+
+    return args
+  end
+
+  # define what happens when the measure is run
+  def run(model, runner, user_arguments)
+    super(model, runner, user_arguments)
+
+    # use the built-in error checking
+    if !runner.validateUserArguments(arguments(model), user_arguments)
+      return false
+    end
+
+    # Assign the user inputs to variables
+    apply_measure = runner.getStringArgumentValue("apply_measure",user_arguments)
+    pressure_drop_reduction_pct = runner.getDoubleArgumentValue("pressure_drop_reduction_pct",user_arguments)
+
+    # Convert the pressure drop reduction to a multiplier
+    pd_mult = (100 - pressure_drop_reduction_pct)/100
+    
+    # This measure is not applicable if apply_measure is false
+    if apply_measure == "FALSE"
+      runner.registerAsNotApplicable("Not Applicable - User chose not to apply this measure via the apply_measure argument.")
+      return true
+    end  
+    
+    # Check arguments for reasonableness
+    if pressure_drop_reduction_pct <= 0 || pressure_drop_reduction_pct >= 100 
+      runner.registerError("Pressure drop reduction percent must be between 0 and 100.")
+      return false
+    end
+
+    # Loop through all air loops, find the fan,
+    # and reduce the pressure drop to model the impact
+    # of lower pressure drop filters.
+    air_loops_pd_lowered = []
+    air_loops = []
+    model.getAirLoopHVACs.each do |air_loop|
+      air_loops << air_loop
+      air_loop.supplyComponents.each do |supply_comp|
+        fan = nil
+        if supply_comp.to_FanConstantVolume.is_initialized
+          fan = supply_comp.to_FanConstantVolume.get
+        elsif supply_comp.to_FanVariableVolume.is_initialized
+          fan = supply_comp.to_FanVariableVolume.get
+        end
+        if !fan.nil?
+          current_pd_pa = fan.pressureRise
+          current_pd_inh2o = OpenStudio.convert(current_pd_pa, "Pa", "inH_{2}O").get
+          new_pd_inh2o = current_pd_inh2o * pd_mult
+          new_pd_pa = OpenStudio.convert(new_pd_inh2o, "inH_{2}O", "Pa").get
+          fan.setPressureRise(new_pd_pa)
+          runner.registerInfo("Lowered pressure drop on #{air_loop.name} by #{pressure_drop_reduction_pct}% from #{current_pd_inh2o.round(1)} in W.C to #{new_pd_inh2o.round(1)} in W.C.")
+          air_loops_pd_lowered << air_loop
+        end
+      end
+    end
+    
+    # Not applicable if no air loops
+    if air_loops.size == 0
+      runner.registerAsNotApplicable("This measure is not applicable because there were no airloops in the building.")
+      return true    
+    end
+    
+    # Not applicable if no airloops were modified
+    if air_loops_pd_lowered.size == 0
+      runner.registerAsNotApplicable("This measure is not applicable because none of the airloops in the model were impacted.")
+      return true
+    end  
+        
+    # Report final condition
+    runner.registerFinalCondition("Lowered fan static pressure on #{air_loops_pd_lowered.size} air loops to reflect improved duct routing.")
+
+    return true
+
+  end
+  
+end
+
+# register the measure to be used by the application
+ImprovedDuctRouting.new.registerWithApplication
