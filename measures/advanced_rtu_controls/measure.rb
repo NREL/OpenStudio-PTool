cb9c54f235cbadffb88e05f94731bf4c8fbb15a5
Brian Ball <Brian.Ball@nrel.gov>
2015-06-04 11:16:01 -0600

    waiting on EMS validation

--- measures/advanced_rtu_controls/measure.rb
+++ measures/advanced_rtu_controls/measure.rb
@@ -43,20 +43,23 @@ class AdvancedRTUControls < OpenStudio::Ruleset::WorkspaceUserScript
   #  end
 
 require 'openstudio'
+require 'json'
 translator = OpenStudio::OSVersion::VersionTranslator.new
-#path = OpenStudio::Path.new(File.dirname(__FILE__) + "/SmallOffice_90_1-2010.osm")
-path = OpenStudio::Path.new(File.dirname(__FILE__) + "/SmallOffice_DOE Ref 1980-2004.osm")
+path = OpenStudio::Path.new(File.dirname(__FILE__) + "/SmallOffice_90_1-2010.osm")
+#path = OpenStudio::Path.new(File.dirname(__FILE__) + "/SmallOffice_DOE Ref 1980-2004.osm")
 #path = OpenStudio::Path.new(File.dirname(__FILE__) + "/SmallOffice_DOE Ref Pre-1980.osm")
 model = translator.loadModel(path)
 model = model.get
 #always_on = model.alwaysOnDiscreteSchedule
-
+results = {}
+airloop_name = []
 # get the cooling coil and fan from a unitary heatpump
 
 model.getAirLoopHVACs.each do |air_loop|
   found_coil = 0  #have not found any cooling coils
   found_hcoil = 0  #have not found any heating coils
   found_fan = 0   #have not found any fans 
+  temp = {}
   air_loop.supplyComponents.each do |component|
     # Get the unitary equipment
     if component.to_AirLoopHVACUnitaryHeatPumpAirToAir.is_initialized
@@ -67,12 +70,14 @@ model.getAirLoopHVACs.each do |air_loop|
         clg_coil = clg_coil.to_CoilCoolingDXSingleSpeed.get
         puts "Found #{clg_coil.name} on #{air_loop.name}"
         found_coil += 1  #found necessary cooling coil DX singlespeed
+        temp[:cool_coil] = "#{clg_coil.name}"
       end
       # get heating coil
       htg_coil = unitary.heatingCoil
       if htg_coil.to_CoilHeatingDXSingleSpeed.is_initialized
         puts "Found #{htg_coil.name} on #{air_loop.name}"
         found_hcoil += 1  #found necessary cooling coil DX singlespeed
+        temp[:heat_coil] = "#{htg_coil.name}"
       end
       # get the supply fan from inside the unitary equipment
       supply_fan = unitary.supplyAirFan
@@ -80,64 +85,81 @@ model.getAirLoopHVACs.each do |air_loop|
         supply_fan = supply_fan.to_FanConstantVolume.get
         puts "Found #{supply_fan.name} on #{air_loop.name}"
         found_fan += 1  #found necessary Fan object
+        temp[:fan] = "#{supply_fan.name}"
       elsif supply_fan.to_FanOnOff.is_initialized
         supply_fan = supply_fan.to_FanOnOff.get
         puts "Found #{supply_fan.name} on #{air_loop.name}"
         found_fan += 1  #found necessary Fan object
-      else 
+        temp[:fan] = "#{supply_fan.name}"
+      else
         puts "No OnOff or Constant Volume Fan in the Unitary system on #{air_loop.name}"      
-      end        
+      end
     end
     # Get the cooling coil directly from the airloop
     if component.to_CoilCoolingDXSingleSpeed.is_initialized
       clg_coil = component.to_CoilCoolingDXSingleSpeed.get
       puts "Found #{clg_coil.name} on #{air_loop.name}"
       found_coil += 1  #found necessary cooling coil DX singlespeed
+      temp[:cool_coil] = "#{clg_coil.name}"
     end
     # Get the heating coil directly from the airloop
     if component.to_CoilHeatingDXSingleSpeed.is_initialized
       htg_coil = component.to_CoilHeatingDXSingleSpeed.get
       puts "Found #{htg_coil.name} on #{air_loop.name}"
       found_hcoil += 1  #found necessary heating coil DX singlespeed
+      temp[:heat_coil] = "#{htg_coil.name}"
     end
     # Get the heating coil directly from the airloop
     if component.to_CoilHeatingGas.is_initialized
       htg_coil = component.to_CoilHeatingGas.get
       puts "Found #{htg_coil.name} on #{air_loop.name}"
       found_hcoil += 1  #found necessary heating coil gas
+      temp[:heat_coil] = "#{htg_coil.name}"
     end
     # Get the heating coil directly from the airloop
     if component.to_CoilHeatingElectric.is_initialized
       htg_coil = component.to_CoilHeatingElectric.get
       puts "Found #{htg_coil.name} on #{air_loop.name}"
       found_hcoil += 1  #found necessary heating coil gas
+      temp[:heat_coil] = "#{htg_coil.name}"
     end
     # get the supply fan directly from the airloop
     if component.to_FanConstantVolume.is_initialized
       supply_fan = component.to_FanConstantVolume.get
       puts "Found #{supply_fan.name} on #{air_loop.name}"
       found_fan += 1  #found necessary Fan object
+      temp[:fan] = "#{supply_fan.name}"
     end
     if component.to_FanOnOff.is_initialized
       supply_fan = component.to_FanOnOff.get
       puts "Found #{supply_fan.name} on #{air_loop.name}"
       found_fan += 1  #found necessary Fan object
-    end 
+      temp[:fan] = "#{supply_fan.name}"
+    end
   end
   puts "airloop #{air_loop.name} found = #{(found_coil + found_fan)} \n"
-  
+  found_oa = 0
+  found_act = 0
+  found_oasch = 0
+  found_oafsch = 0
   #found too many objects on an airloop
-  if (found_coil + found_fan) > 2
-    puts "Too many objects on airloop #{air_loop.name}"
+  if (found_coil + found_hcoil + found_fan) > 3
+    puts "Too many objects on airloop #{air_loop.name}. Airloop N/A"
   #found a Fan and Cooling Coil DX Single Speed, get rest of info
-  elsif (found_coil + found_fan) == 2 
+  elsif (found_coil + found_hcoil + found_fan) < 3
+    puts "Not enough objects on airloop #{air_loop.name}. Airloop N/A"
+  elsif (found_coil + found_hcoil + found_fan) == 3 
       # get outdoorair controller
       if air_loop.airLoopHVACOutdoorAirSystem.is_initialized
         controller_oa = air_loop.airLoopHVACOutdoorAirSystem.get.getControllerOutdoorAir
         puts "Found #{controller_oa.name} on #{air_loop.name}"
+        found_oa += 1 #found necessary OA controller
+        temp[:controller_oa] = "#{controller_oa.name}"
         # get actuator node name
         actuatorNodeName = air_loop.airLoopHVACOutdoorAirSystem.get.outboardOANode.get.name.get
         puts "Found #{actuatorNodeName} on #{air_loop.name}" 
+        found_act += 1  #found necessary actuator node
+        temp[:actuatorNodeName] = "#{actuatorNodeName}"
         # get minimumFractionofOutdoorAirSchedule
         minimumFractionofOutdoorAirSchedule = controller_oa.minimumFractionofOutdoorAirSchedule
         # get minimumOutdoorAirSchedule
@@ -147,62 +169,180 @@ model.getAirLoopHVACs.each do |air_loop|
         end
         if minimumFractionofOutdoorAirSchedule.is_initialized
           puts "Found #{minimumFractionofOutdoorAirSchedule.get.name} on #{air_loop.name}"
+          found_oafsch += 1 #found necessary fraction OA schedule
+          temp[:minimumFractionofOutdoorAirSchedule] = "#{minimumFractionofOutdoorAirSchedule.get.name}"
         else
           always_on = model.alwaysOnDiscreteSchedule
           controller_oa.setMinimumFractionofOutdoorAirSchedule(always_on)
           puts "Added #{controller_oa.minimumFractionofOutdoorAirSchedule.get.name} on #{air_loop.name}"
-        end       
+          found_oafsch += 1  #added necessary fraction OA schedule
+          temp[:minimumFractionofOutdoorAirSchedule] = "#{controller_oa.minimumFractionofOutdoorAirSchedule.get.name}"
+        end
         if minimumOutdoorAirSchedule.is_initialized
           puts "Found #{minimumOutdoorAirSchedule.get.name} on #{air_loop.name}"
+          found_oasch += 1 #found necessary OA schedule
+          temp[:minimumOutdoorAirSchedule] = "#{minimumOutdoorAirSchedule.get.name}"
         else
           always_on = model.alwaysOnDiscreteSchedule
           controller_oa.setMinimumOutdoorAirSchedule(always_on) 
-          puts "Added #{controller_oa.minimumOutdoorAirSchedule.get.name} on #{air_loop.name}"          
-        end  
+          puts "Added #{controller_oa.minimumOutdoorAirSchedule.get.name} on #{air_loop.name}"
+          found_oasch += 1 #added necessary OA schedule
+          temp[:minimumOutdoorAirSchedule] = "#{controller_oa.minimumOutdoorAirSchedule.get.name}"
+        end
       end
-  end
-  puts "\n"
-end  
-  
-  
-nodes = [1,2,3]
-ems_string = ""
-  
-ems_string << "EnergyManagementSystem:GlobalVariable," + "\n"
-ems_string << "	FanPwrExp,  ! Exponent used in fan power law" + "\n"
-ems_string << "	Stage1Speed,  ! Fan speed in cooling mode" + "\n"
-ems_string << "	HeatSpeed,    ! Fan speed in heating mode" + "\n"
-ems_string << "	VenSpeed,   ! Fan speed in ventilation mode" + "\n"
-ems_string << "	EcoSpeed; ! Fan speed in economizer mode" + "\n"
-ems_string << "\n"
-ems_string << "EnergyManagementSystem:Program," + "\n"
-ems_string << "	Set_FanCtl_Par1," + "\n"
-ems_string << "	SET FanPwrExp = 2.2," + "\n"
-ems_string << "	SET HeatSpeed = 0.9," + "\n"
-ems_string << "	SET VenSpeed = 0.4," + "\n"
-ems_string << "	SET Stage1Speed = 0.9," + "\n"
-ems_string << "	SET EcoSpeed = 0.75;" + "\n"
-ems_string << "\n"
-ems_string << "EnergyManagementSystem:Program," + "\n"
-ems_string << "	Set_FanCtl_Par2," + "\n"
-nodes.each_with_index do |item, i|
-  if i < nodes.size - 1
-  ems_string << "SET PSZ#{i}_OADesignMass = PSZ#{i}_DesignFlowMass," + "\n"
-  else
-  ems_string << "SET PSZ#{i}_OADesignMass = PSZ#{i}_DesignFlowMass;" + "\n"
+      if (found_oasch + found_oafsch + found_act + found_oa) == 4  #add valid air loop to results
+        results["#{air_loop.name}"] = temp
+        airloop_name << "#{air_loop.name}"
+        puts "Adding valid AirLoop #{air_loop.name} to results. \n"
+      end  
   end
 end
+
+#results[results.keys[1].to_s][:cool_coil]  
+
+ems_string = ""
+
+ems_string << "EnergyManagementSystem:GlobalVariable," + "\n"
+ems_string << " FanPwrExp,  ! Exponent used in fan power law" + "\n"
+ems_string << " Stage1Speed,  ! Fan speed in cooling mode" + "\n"
+ems_string << " HeatSpeed,    ! Fan speed in heating mode" + "\n"
+ems_string << " VenSpeed,   ! Fan speed in ventilation mode" + "\n"
+ems_string << " EcoSpeed; ! Fan speed in economizer mode" + "\n"
+ems_string << "\n"
+ems_string << "EnergyManagementSystem:Program," + "\n"
+ems_string << " Set_FanCtl_Par1," + "\n"
+ems_string << " SET FanPwrExp = 2.2," + "\n"
+ems_string << " SET HeatSpeed = 0.9," + "\n"
+ems_string << " SET VenSpeed = 0.4," + "\n"
+ems_string << " SET Stage1Speed = 0.9," + "\n"
+ems_string << " SET EcoSpeed = 0.75;" + "\n"
+ems_string << "\n"
+ems_string << "EnergyManagementSystem:Program," + "\n"
+ems_string << " Set_FanCtl_Par2," + "\n"
+
+results.each_with_index do |(key, value), i|  
+  if i < results.size - 1
+  ems_string << " SET PSZ#{i}_OADesignMass = PSZ#{i}_DesignFlowMass," + "\n"
+  else
+  ems_string << " SET PSZ#{i}_OADesignMass = PSZ#{i}_DesignFlowMass;" + "\n"
+  end
+end
+
 ems_string << "\n"
 ems_string << "EnergyManagementSystem:ProgramCallingManager," + "\n"
-ems_string << "	Fan_Parameter_manager,  !- Name" + "\n"
-ems_string << "	BeginNewEnvironment,  !- EnergyPlus Model Calling Point" + "\n"
-ems_string << "	Set_FanCtl_Par1,        !- Program Name 1" + "\n"
-ems_string << "	Set_FanCtl_Par2;        !- Program Name 1" + "\n"
+ems_string << " Fan_Parameter_manager,  !- Name" + "\n"
+ems_string << " BeginNewEnvironment,  !- EnergyPlus Model Calling Point" + "\n"
+ems_string << " Set_FanCtl_Par1,        !- Program Name 1" + "\n"
+ems_string << " Set_FanCtl_Par2;        !- Program Name 1" + "\n"
 ems_string << "\n"
-  
+
+results.each_with_index do |(key, value), i|
+ems_string << "EnergyManagementSystem:InternalVariable," + "\n"
+ems_string << " PSZ#{i}_DesignFlowMass, !- Name " + "\n"
+ems_string << " #{value[:controller_oa]}, !- Internal Data Index Key Name" + "\n"
+ems_string << " Outdoor Air Controller Minimum Mass Flow Rate; !- Internal Data Type" + "\n"
+ems_string << "\n"
+end
+
+results.each_with_index do |(key, value), i|
+ems_string << "EnergyManagementSystem:InternalVariable," + "\n"
+ems_string << " PSZ#{i}_FanDesignPressure, !- Name " + "\n"
+ems_string << " #{value[:fan]}, !- Internal Data Index Key Name" + "\n"
+ems_string << " Fan Nominal Pressure Rise; !- Internal Data Type" + "\n"
+ems_string << "\n"
+ems_string << "EnergyManagementSystem:InternalVariable," + "\n"
+ems_string << " PSZ#{i}_DesignFlowMass, !- Name " + "\n"
+ems_string << " #{value[:controller_oa]}, !- Internal Data Index Key Name" + "\n"
+ems_string << " Outdoor Air Controller Maximum Mass Flow Rate; !- Internal Data Type" + "\n"
+ems_string << "\n"
+ems_string << "EnergyManagementSystem:Sensor," + "\n"
+ems_string << " PSZ#{i}_OASch," + "\n"
+ems_string << " #{value[:minimumOutdoorAirSchedule]}," + "\n"
+ems_string << " Schedule Value;" + "\n"
+ems_string << "\n"
+ems_string << "EnergyManagementSystem:Sensor," + "\n"
+ems_string << " PSZ#{i}_OAFracSch," + "\n"
+ems_string << " #{value[:minimumFractionofOutdoorAirSchedule]}," + "\n"
+ems_string << " Schedule Value;" + "\n"
+ems_string << "\n"
+ems_string << "EnergyManagementSystem:Sensor," + "\n"
+ems_string << " PSZ#{i}_OAFlowMass," + "\n"
+ems_string << " #{value[:actuatorNodeName]}," + "\n"
+ems_string << " System Node Mass Flow Rate;" + "\n"
+ems_string << "\n"
+ems_string << "EnergyManagementSystem:Sensor," + "\n"
+ems_string << " PSZ#{i}_HtgRTF," + "\n"
+ems_string << " #{value[:heat_coil]}," + "\n"
+ems_string << " Heating Coil Runtime Fraction;" + "\n"
+ems_string << "\n"
+ems_string << "EnergyManagementSystem:Sensor," + "\n"
+ems_string << " PSZ#{i}_ClgRTF," + "\n"
+ems_string << " #{value[:cool_coil]}," + "\n"
+ems_string << " Cooling Coil Runtime Fraction;" + "\n"
+ems_string << "\n"
+ems_string << "EnergyManagementSystem:Actuator," + "\n"
+ems_string << " PSZ#{i}_FanPressure, ! Name " + "\n"
+ems_string << " #{value[:fan]}, ! Actuated Component Unique Name" + "\n"
+ems_string << " Fan, ! Actuated Component Type" + "\n"
+ems_string << " Fan Pressure Rise; ! Actuated Component Control Type" + "\n"
+ems_string << "\n"
+end
+
+results.each_with_index do |(key, value), i|
+ems_string << "EnergyManagementSystem:Program," + "\n"
+ems_string << " PSZ#{i}_FanControl,        !- Name" + "\n"
+ems_string << " IF PSZ#{i}_HtgRTF > 0," + "\n"
+ems_string << "  SET PSZ#{i}_Htg = PSZ#{i}_HtgRTF,      ! Percent of time in heating mode" + "\n"
+ems_string << "  SET PSZ#{i}_Ven = 1 - PSZ#{i}_HtgRTF,  ! Percent of time in ventilation mode" + "\n"
+ems_string << "  SET PSZ#{i}_Eco = 0,       ! Percent of time in economizer mode" + "\n"
+ems_string << "  SET PSZ#{i}_Stage1 = 0,    ! Percent of time in DX cooling" + "\n"
+ems_string << " ELSE," + "\n"
+ems_string << "  SET PSZ#{i}_Htg = 0," + "\n"
+ems_string << "  SET PSZ#{i}_MinOA1 = PSZ#{i}_OADesignMass * PSZ#{i}_OASch," + "\n"
+ems_string << "  SET PSZ#{i}_MinOA2 = PSZ#{i}_DesignFlowMass * PSZ#{i}_OAFracSch," + "\n"
+ems_string << "  SET PSZ#{i}_MinOA = @Max PSZ#{i}_MinOA1 PSZ#{i}_MinOA2,  " + "\n"
+ems_string << "  IF  PSZ#{i}_ClgRTF > 0,    ! Mechanical cooling is on" + "\n"
+ems_string << "   SET PSZ#{i}_Stage1 = PSZ#{i}_ClgRTF," + "\n"
+ems_string << "   IF PSZ#{i}_OAFlowMass > PSZ#{i}_MinOA,  ! Integrated Economzing mode" + "\n"
+ems_string << "    SET PSZ#{i}_Eco = 1-PSZ#{i}_ClgRTF,  " + "\n"
+ems_string << "    SET PSZ#{i}_Ven = 0," + "\n"
+ems_string << "   ELSE," + "\n"
+ems_string << "    SET PSZ#{i}_Eco = 0," + "\n"
+ems_string << "    SET PSZ#{i}_Ven = 1-PSZ#{i}_ClgRTF," + "\n"
+ems_string << "   ENDIF," + "\n"
+ems_string << "  ELSE,               ! Mechanical cooling is off" + "\n"
+ems_string << "   SET PSZ#{i}_Stage1 = 0, " + "\n"
+ems_string << "   IF PSZ#{i}_OAFlowMass > PSZ#{i}_MinOA,  ! Economizer mode" + "\n"
+ems_string << "    SET PSZ#{i}_Eco = 1.0," + "\n"
+ems_string << "    SET PSZ#{i}_Ven = 0," + "\n"
+ems_string << "   ELSE," + "\n"
+ems_string << "    SET PSZ#{i}_Eco = 0," + "\n"
+ems_string << "    SET PSZ#{i}_Ven = 1.0," + "\n"
+ems_string << "   ENDIF," + "\n"
+ems_string << "  ENDIF," + "\n"
+ems_string << " ENDIF," + "\n"
+ems_string << "\n"
+ems_string << " ! For each mode, (percent time in mode) * (fanSpeed^PwrExp) is the contribution to weighted fan power over time step" + "\n"
+ems_string << " SET PSZ#{i}_FPR = PSZ#{i}_Ven * (VenSpeed ^ FanPwrExp)," + "\n"
+ems_string << " SET PSZ#{i}_FPR = PSZ#{i}_FPR + PSZ#{i}_Eco * (EcoSpeed ^ FanPwrExp)," + "\n"
+ems_string << " SET PSZ#{i}_FPR1 = PSZ#{i}_Stage1 * (Stage1Speed ^ FanPwrExp)," + "\n"
+ems_string << " SET PSZ#{i}_FPR = PSZ#{i}_FPR + PSZ#{i}_FPR1," + "\n"
+ems_string << " SET PSZ#{i}_FPR3 = PSZ#{i}_Htg * (HeatSpeed ^ FanPwrExp)," + "\n"
+ems_string << " SET PSZ#{i}_FanPwrRatio = PSZ#{i}_FPR +  PSZ#{i}_FPR3," + "\n"
+ems_string << "\n"
+ems_string << "! System fan power is directly proportional to static pressure, so this change linearly adjusts fan energy for speed control" + "\n"
+ems_string << " SET PSZ#{i}_FanPressure = PSZ#{i}_FanDesignPressure * PSZ#{i}_FanPwrRatio;" + "\n"
+ems_string << "\n"
+ems_string << "EnergyManagementSystem:ProgramCallingManager," + "\n"
+ems_string << " PSZ#{i}_Fan_Manager,   !- Name" + "\n"
+ems_string << " BeginTimestepBeforePredictor,  !- EnergyPlus Model Calling Point" + "\n"
+ems_string << " PSZ#{i}_FanControl;        !- Program Name 1" + "\n"
+ems_string << "\n"
+end
+
 File.open("ems_1", "w") do |f|
   f.write(ems_string)
-end  
+end
     #unique initial conditions based on
     # removed listing ranges for variable values since we are editing multiple fields vs. a single field.
     runner.registerInitialCondition("The building has #{emsProgram.size} EMS objects.")
