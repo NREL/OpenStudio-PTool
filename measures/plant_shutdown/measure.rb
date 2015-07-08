8eac9163ae704599b0f4285b1db68175547daf39
Brian Ball <Brian.Ball@nrel.gov>
2015-07-08 11:17:38 -0600

    commiting design doc added info / initial / final / na messages

--- measures/plant_shutdown/measure.rb
+++ measures/plant_shutdown/measure.rb
@@ -48,52 +48,74 @@ class PlantShutdown < OpenStudio::Ruleset::ModelUserScript
     end
     
     require 'json'
-    results = {}
     
-    def pump_control(pump, runner)
+    def pump_control(pump, pumps, runner)
+      changed = false
       runner.registerInfo("pump control type #{pump.pumpControlType}")
       if pump.pumpControlType == "Continuous"
         runner.registerInfo("setting pump #{pump.name.to_s} control type to Intermittent")
         pump.setPumpControlType("Intermittent")
+        pumps << pump.name.to_s
+        changed = true
       end
       if pump.pumpFlowRateSchedule.is_initialized
         runner.registerInfo("resetting pump #{pump.name.to_s} pumpFlowRateSchedule")
         pump.resetPumpFlowRateSchedule
       end
+      return changed
     end
 
-    def check_supply_side(plantLoop, i, runner)
+    def check_supply_side(plantLoop, i, pumps, runner)
+      changed = false
       plantLoop.supplyComponents.each_with_index do |comp, index|
         pump = comp.to_PumpConstantSpeed
         if pump.is_initialized
           runner.registerInfo("plant loop #{i.to_s} has constant pump")
-          pump_control(pump.get, runner)
+          changed = pump_control(pump.get, pumps, runner)
         end
         pump = comp.to_PumpVariableSpeed
         if pump.is_initialized
           runner.registerInfo("plant loop #{i.to_s} has variable pump")
-          pump_control(pump.get, runner)
+          changed = pump_control(pump.get, pumps, runner)
         end
       end
+      return changed
     end
 
+    results = []
+    skipped = []
+    na = []
+    pumps = []
+
     model.getPlantLoops.each_with_index do |plantLoop, index|
+      changed = false
       skip = false
       plantLoop.demandComponents.each do |comp|
         if comp.to_WaterUseConnections.is_initialized
           runner.registerInfo("plant loop #{index.to_s} uses water, skipping")
           skip = true
+          skipped << plantLoop.name.to_s
         end
         break if skip == true
       end
-      check_supply_side(plantLoop, index, runner) if skip == false
+      changed = check_supply_side(plantLoop, index, pumps, runner) if skip == false
+      if skip == false
+        changed == true ? results << plantLoop.name.to_s : na << plantLoop.name.to_s
+      end  
     end
         
     #unique initial conditions based on
-    runner.registerInitialCondition("The building has #{results.length} constant air volume units for which this measure is applicable.")
+    if !results.empty?
+      runner.registerInitialCondition("The initial model has #{results.length} pumps set to operate continuously; this measure is applicable.")
+    end
 
+    if results.empty?
+      runner.registerAsNotApplicable("No continuously operating chilled-water loop, hot-water loop, or condenser loop pumps were found. EEM not applied")
+      return true
+    end
+    
     #reporting final condition of model
-    runner.registerFinalCondition("VSDs and associated controls were applied to  #{results.length} single-zone, constant air volume units in the model.")
+    runner.registerFinalCondition("The following pumps were set to operate intermittently: #{results} \n The following pumps were skipped: #{skipped} \n The following pumps were not applicable: #{na}")
     return true
 
   end #end the run method
