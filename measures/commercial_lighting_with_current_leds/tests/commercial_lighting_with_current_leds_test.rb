79edefe2cf30dcb2c6a436f35ba06d429a47dd37
unknown <aparker@aparker-26487s.nrel.gov>
2015-09-16 15:51:35 -0600

    Adds current LED measure

---
+++ measures/commercial_lighting_with_current_leds/tests/commercial_lighting_with_current_leds_test.rb
@@ -0,0 +1,69 @@
+require 'openstudio'
+require 'openstudio/ruleset/ShowRunnerOutput'
+require 'minitest/autorun'
+
+require_relative '../measure.rb'
+
+require 'fileutils'
+
+class CommercialLightingWithCurrentLEDsTest < MiniTest::Unit::TestCase
+
+  def test_small_office
+     
+    # Create an instance of the measure
+    measure = CommercialLightingWithCurrentLEDs.new
+    
+    # Create an instance of a runner
+    runner = OpenStudio::Ruleset::OSRunner.new
+    
+    # Load the test model
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    path = OpenStudio::Path.new(File.dirname(__FILE__) + "/small_office_1980-2004.osm")
+    model = translator.loadModel(path)
+    assert((not model.empty?))
+    model = model.get
+    
+    # Get arguments and test that they are what we are expecting
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Ruleset::OSArgumentMap.new 
+
+    # Run the measure
+    measure.run(model, runner, argument_map)
+    result = runner.result
+    show_output(result)
+
+    # Ensure the measure finished as expected
+    assert(result.value.valueName == "Success")
+      
+  end
+
+  def test_secondary_school
+     
+    # Create an instance of the measure
+    measure = CommercialLightingWithCurrentLEDs.new
+    
+    # Create an instance of a runner
+    runner = OpenStudio::Ruleset::OSRunner.new
+    
+    # Load the test model
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    path = OpenStudio::Path.new(File.dirname(__FILE__) + "/secondary_school_90.1-2010.osm")
+    model = translator.loadModel(path)
+    assert((not model.empty?))
+    model = model.get
+    
+    # Get arguments and test that they are what we are expecting
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Ruleset::OSArgumentMap.new
+
+    # Run the measure
+    measure.run(model, runner, argument_map)
+    result = runner.result
+    show_output(result)
+
+    # Ensure the measure finished as expected
+    assert(result.value.valueName == "Success")
+      
+  end
+
+end
