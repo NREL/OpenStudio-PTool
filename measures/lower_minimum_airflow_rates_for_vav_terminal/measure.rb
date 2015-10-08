bda5b74ebc24dcc2073989ee074f114f45dde0e9
chrisbalbach <cbalbach@psdconsulting.com>
2015-10-08 10:31:49 -0400

    Updated comments in code for minimum airflow rates for vav terminal measure.rb file

--- measures/lower_minimum_airflow_rates_for_vav_terminal/measure.rb
+++ measures/lower_minimum_airflow_rates_for_vav_terminal/measure.rb
@@ -24,71 +24,67 @@ class LowerMinimumAirflowRatesForVAVTerminal < OpenStudio::Ruleset::ModelUserScr
 		args = OpenStudio::Ruleset::OSArgumentVector.new
 		return args
 	end #ends the argument
-
-
-		
 		
 	# define what happens when the measure is run
 	def run(model, runner, user_arguments)
 		super(model, runner, user_arguments)
-
 			
-			def neat_numbers(number, roundto = 2) #round to 0 or 2)
-				if roundto == 2
-					number = sprintf "%.2f", number
-				else
-					number = number.round
-				end
-					#regex to add commas
-					number.to_s.reverse.gsub(%r{([0-9]{3}(?=([0-9])))}, "\\1,").reverse
-			end #end def neat_numbers
-			
-			
-			
+		def neat_numbers(number, roundto = 2) #round to 0 or 2)
+			if roundto == 2
+				number = sprintf "%.2f", number
+			else
+				number = number.round
+			end
+				#regex to add commas
+				number.to_s.reverse.gsub(%r{([0-9]{3}(?=([0-9])))}, "\\1,").reverse
+		end #end def neat_numbers
 			
 		# use the built-in error checking
 		if !runner.validateUserArguments(arguments(model), user_arguments)
 			return false
 		end
 
-		# initialize the variables
+		# initialize counter variables
 		numb_airsingle_terminal_vavreheat = 0
 		numb_airsingle_terminal_vavnoreheat = 0
-			
-		all_airloops = model.getAirLoopHVACs   # retrieving all the airloops
+		
+		# Get all airloops from the model
+		all_airloops = model.getAirLoopHVACs   
 
 		if all_airloops.size == 0
-			runner.registerAsNotApplicable("Model has no airloops. Measure is not applicable.") 
+			runner.registerAsNotApplicable("This model has no airloops. The measure is not applicable.") 
 			return true
 		end
 
-		#loop through air loops
+		#loop through each airloop object
 		all_airloops.each do |loop| 
+			# Retrieve the thermal zone objects associated with each airloop
 			thermal_zones = loop.thermalZones
-
 			
-			#find thermal zone attached to each air loop
+			#Loop through the thermal zone objects attached to each airloop
 			thermal_zones.each do |thermal_zone|
 				  
-				# get the thermal zone area
+				# Retrieve the area of the thermal zone 
 				m2_thermal_zone_area = thermal_zone.floorArea
-				
+				# convert metric floor area into floor area using IP units and round
 				sqft_thermal_zone_area = OpenStudio.convert(m2_thermal_zone_area,"m^2","ft^2").get
 				ft2_thermal_zone_area = neat_numbers(sqft_thermal_zone_area,2)				
-				# calc the minimum airflow rate needed for the zone
+				# calculate the minimum airflow rate needed for the zone
 				thermal_zone_min_flow = (sqft_thermal_zone_area * 0.4)
-				
-		
-				
-				#get the VAV boxes for the thermal zone
+							
+				# Retrieve all ZoneEquipment objects attached to the thermal zone
 				zone_equip = thermal_zone.equipment
 				 
+				# Loop through each ZoneEquipment objects 
 				zone_equip.each do |vav_box|
+					# Attempt to map the ZoneEquipment object to a Single Duct VAV box (No Reheat or Reheat) object type
 					airterminal_singleduct_vavreheat = vav_box.to_AirTerminalSingleDuctVAVReheat
 					airterminal_singleduct_vavnoreheat = vav_box.to_AirTerminalSingleDuctVAVNoReheat   #alter equipment of the correct type
-							
+					
+					# If ZoneEquipment object = type single duct VAV with reheat then execute this logic			
 					if not airterminal_singleduct_vavreheat.empty?
 						airterminal_singleduct_vavreheat = airterminal_singleduct_vavreheat.get
+						#retrieve and store existing method for ZoneMinimumAirflowRate from the SingeDuctVAVReheat box object
 						existing_method = airterminal_singleduct_vavreheat.zoneMinimumAirFlowMethod
 						if existing_method == "FixedFlowRate" 
 							existing_cubic_mps = airterminal_singleduct_vavreheat.fixedMinimumAirFlowRate
@@ -99,6 +95,7 @@ class LowerMinimumAirflowRatesForVAVTerminal < OpenStudio::Ruleset::ModelUserScr
 							existing_cons_air_frac = airterminal_singleduct_vavreheat.constantMinimumAirFlowFraction
 							existing_cons_air_frac = neat_numbers(existing_cons_air_frac,2)
 						end
+						
 						#change the zoneMinimumAirFlowMethod
 						airterminal_singleduct_vavreheat.setZoneMinimumAirFlowMethod("FixedFlowRate")
 						airterminal_singleduct_vavreheat.setFixedMinimumAirFlowRate(thermal_zone_min_flow)
@@ -110,42 +107,37 @@ class LowerMinimumAirflowRatesForVAVTerminal < OpenStudio::Ruleset::ModelUserScr
 						#increment counter of above object by one
 						numb_airsingle_terminal_vavreheat += 1
 										
-					end						
+					end	# end logic block for single duct VAV with reheat 
 											
-						if not airterminal_singleduct_vavnoreheat.empty?
-							airterminal_singleduct_vavnoreheat = airterminal_singleduct_vavnoreheat.get
-							existing_method_2 = airterminal_singleduct_vavnoreheat.zoneMinimumAirFlowInputMethod
-							if existing_method_2.to_s == "FixedFlowRate" 
-								existing_cubic_mps_2 = airterminal_singleduct_vavnoreheat.fixedMinimumAirFlowRate 
-								
-								if not existing_cubic_mps_2.empty?
-									existing_cfm_2 = OpenStudio.convert(existing_cubic_mps_2.get,"m^3/s","cfm")
-									existing_cfm_2 = neat_numbers(existing_cfm_2,2)
-								end
+					# If ZoneEquipment object = type single duct VAV without reheat then execute this logic						
+					if not airterminal_singleduct_vavnoreheat.empty?
+						airterminal_singleduct_vavnoreheat = airterminal_singleduct_vavnoreheat.get
+						existing_method_2 = airterminal_singleduct_vavnoreheat.zoneMinimumAirFlowInputMethod
+						if existing_method_2.to_s == "FixedFlowRate" 
+							existing_cubic_mps_2 = airterminal_singleduct_vavnoreheat.fixedMinimumAirFlowRate 
+							
+							if not existing_cubic_mps_2.empty?
+								existing_cfm_2 = OpenStudio.convert(existing_cubic_mps_2.get,"m^3/s","cfm")
+								existing_cfm_2 = neat_numbers(existing_cfm_2,2)
 							end
-							if existing_method_2.to_s == "Constant" 
-								existing_cons_air_frac_2 = airterminal_singleduct_vavnoreheat.constantMinimumAirFlowFraction
-								existing_cons_air_frac_2 = neat_numbers(existing_cons_air_frac_2,2)
-							end
-							#change the zoneMinimumAirFlowMethod
-							airterminal_singleduct_vavnoreheat.setZoneMinimumAirFlowInputMethod("FixedFlowRate")
-							airterminal_singleduct_vavnoreheat.setFixedMinimumAirFlowRate(thermal_zone_min_flow)
-							cubic_mps_2 = airterminal_singleduct_vavnoreheat.fixedMinimumAirFlowRate
-							cubic_mps_2 = neat_numbers(cubic_mps_2,2)
-							#short def to make numbers pretty (converts 4125001.25641 to 4,125,001.26 or 4,125,001). The definition be called through this measure
-							atsdvnr = airterminal_singleduct_vavnoreheat.name.get
-							runner.registerInfo("Minimum Airflow rate for Single duct VAV with no reheat named '#{atsdvnr}' with area #{ft2_thermal_zone_area} sqft, & zone minimum air flow input method as '#{existing_method_2}' = #{existing_cfm_2} #{existing_cons_air_frac_2} has been changed to a minimum fixed flow rate of #{cubic_mps_2} cfm.")
-							#increment counter of above object by one
-							numb_airsingle_terminal_vavnoreheat += 1
-										
-						end		
-							
-							
-							
-				
-											
-					#end		#ends the airterminal if statement			
-								
+						end
+						if existing_method_2.to_s == "Constant" 
+							existing_cons_air_frac_2 = airterminal_singleduct_vavnoreheat.constantMinimumAirFlowFraction
+							existing_cons_air_frac_2 = neat_numbers(existing_cons_air_frac_2,2)
+						end
+						#change the zoneMinimumAirFlowMethod
+						airterminal_singleduct_vavnoreheat.setZoneMinimumAirFlowInputMethod("FixedFlowRate")
+						airterminal_singleduct_vavnoreheat.setFixedMinimumAirFlowRate(thermal_zone_min_flow)
+						cubic_mps_2 = airterminal_singleduct_vavnoreheat.fixedMinimumAirFlowRate
+						cubic_mps_2 = neat_numbers(cubic_mps_2,2)
+						#short def to make numbers pretty (converts 4125001.25641 to 4,125,001.26 or 4,125,001). The definition be called through this measure
+						atsdvnr = airterminal_singleduct_vavnoreheat.name.get
+						runner.registerInfo("Minimum Airflow rate for Single duct VAV with no reheat named '#{atsdvnr}' with area #{ft2_thermal_zone_area} sqft, & zone minimum air flow input method as '#{existing_method_2}' = #{existing_cfm_2} #{existing_cons_air_frac_2} has been changed to a minimum fixed flow rate of #{cubic_mps_2} cfm.")
+						#increment counter of above object by one
+						numb_airsingle_terminal_vavnoreheat += 1
+									
+					end	# end logic block for single duct VAV without reheat 	
+		
 				end #ends the do loop for zone equipments
 					
 			end #ends the do loop for thermal zones
@@ -157,14 +149,13 @@ class LowerMinimumAirflowRatesForVAVTerminal < OpenStudio::Ruleset::ModelUserScr
 		# report AsNotApplicable condition of model	
 		if	
 			total_modified_objects == 0
-			runner.registerAsNotApplicable("The building contains no qualified objects. Measure is not applicable.") 	
+			runner.registerAsNotApplicable("The building contains no qualified single duct VAV objects. Measure is not applicable.") 	
 			return true
 		end
 			
 		# report initial condition of model
 		runner.registerInitialCondition("The model begins with #{numb_airsingle_terminal_vavreheat} Single duct VAV with reheat objects & #{numb_airsingle_terminal_vavnoreheat} Single duct VAV with no reheat objects.") # report initial condition of model
-
-		   
+	   
 		# report final condition of model
 		runner.registerFinalCondition("The model finished with #{total_modified_objects} objects having the 'zone minimum air flow method' set to 'fixed flow rate' and a minimum airflow rate of 0.4 cfm/sf.")  
 			
@@ -173,8 +164,6 @@ class LowerMinimumAirflowRatesForVAVTerminal < OpenStudio::Ruleset::ModelUserScr
 	end #ends the run method
 
 end #ends the class
-
-
 
 # register the measure to be used by the application
 LowerMinimumAirflowRatesForVAVTerminal.new.registerWithApplication
