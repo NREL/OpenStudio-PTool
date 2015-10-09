a9876dc5a4de366b764310a04f30bf0e1553592a
chrisbalbach <cbalbach@psdconsulting.com>
2015-10-09 16:15:08 -0400

    Editied widen thermostat setpoint meaure.rb and specification document

--- measures/widen_thermostat_setpoint/measure.rb
+++ measures/widen_thermostat_setpoint/measure.rb
@@ -36,147 +36,136 @@ class WidenThermostatSetpoint < OpenStudio::Ruleset::ModelUserScript
 	
 	#initialize variables
 	tz_count = 0
-	
-	# retrieve all thermal zones
-	thermal_zones = model.getThermalZones
-	@thermal_zone_array = []
-	#loop through each thermal zone
-	thermal_zones.each do |thermal_zone|
-		@thermal_zone_array << thermal_zone
+	clg_tstat_schedule = []
+	thermal_zone_array = []
 		
-		# test to see if thermal zone has a thernmostat object assigned or is unconditioned. 
+	# get the thermal zones and loop through them 
+	model.getThermalZones.each do |thermal_zone|
+	
+		thermal_zone_array << thermal_zone
+		
+		# test to see if thermal zone has a thermostat object assigned or is unconditioned. 
 		if thermal_zone.thermostatSetpointDualSetpoint.is_initialized
-			@zone_thermostat = thermal_zone.thermostatSetpointDualSetpoint.get
+			zone_thermostat = thermal_zone.thermostatSetpointDualSetpoint.get
 			tz_count += 1
-		else
-			runner.registerInfo("The thermal zone named #{thermal_zone.name} does not have a dual setpoint thermostat object associated with it. The measure will not modify any attributes related to this zone.")
-		end
-		
-		if @zone_thermostat.coolingSetpointTemperatureSchedule.is_initialized
-			@clg_tstat_schedule = @zone_thermostat.coolingSetpointTemperatureSchedule.get
-		end	
 			
-		# clone the existing schedule in case it is used somewhere else in the model
-		@cloned_clg_tstat_schedule = @clg_tstat_schedule.clone
-		cloned_clg_tstat_schedule.name("#{clg_tstat_schedule.name} modified cooling T-stat schedule.")
-		
-		if @cloned_clg_tstat_schedule.to_ScheduleRuleset.is_initialized
-			@schedule = @cloned_clg_tstat_schedule.to_ScheduleRuleset.get
-
-			# gather profiles of cloned schedule
-			@profiles = []
-			@cooling_thermostat_array = []
-			defaultProfile = @schedule.to_ScheduleRuleset.get.defaultDaySchedule
-			
-			@profiles << defaultProfile
-			rules = @schedule.scheduleRules
-			
-			rules.each do |rule|
-				@profiles << rule.daySchedule
-			end # end the rule do
-
-			#adjust profiles of temperature schedule of cloned schedule by + 1.5 deg F delta (0.833 C)
-			@profiles.each do |profile|
-				time = profile.times
-				i = 0
-				#TODO - name new profile
-				profile.values.each do |value|
-			
-					delta = 0.8333
-					new_value = value + delta
-					
-					profile.addValue(time[i], new_value)
-					i += 1
-					@new_clg_tstat_schedule_name = ("#{@clg_tstat_schedule.name}+1.5F")
-					@cloned_clg_tstat_schedule.setName(@new_clg_tstat_schedule_name)
-					@cooling_thermostat_array << @cloned_clg_tstat_schedule
-					runner.registerInfo("The existing cooling thermostat '#{@clg_tstat_schedule.name}' has been changed to '#{@new_clg_tstat_schedule_name}'.")
-					
-				end # end loop through each profile values
+			if zone_thermostat.coolingSetpointTemperatureSchedule.is_initialized
+				clg_tstat_schedule = zone_thermostat.coolingSetpointTemperatureSchedule.get
 				
-			end # end loop through each profile
+				# clone the existing cooling T-stat schedule in case it is used somewhere else in the model
+				cloned_clg_tstat_schedule = clg_tstat_schedule.clone
+				@new_clg_tstat_schedule_name = ("#{clg_tstat_schedule.name}+1.5F")
 			
-		end # end if statement for changing schedule
-			
-		if @zone_thermostat.heatingSetpointTemperatureSchedule.is_initialized
-			@htg_tstat_schedule = @zone_thermostat.heatingSetpointTemperatureSchedule.get
-					
-			# clone the existing heating T-stat schedule in case it is used somewhere else
-			@cloned_htg_tstat_schedule = @htg_tstat_schedule.clone
-			
-			#name cloned heating t-stat schedule
-			cloned_htg_tstat_schedule.name("#{htg_tstat_schedule.name} modified heating T-stat schedule.")
-
-			if @cloned_htg_tstat_schedule.to_ScheduleRuleset.is_initialized
-				@schedule = @cloned_htg_tstat_schedule.to_ScheduleRuleset.get
-			
-				# gather profiles of cloned schedule
-				@profiles_h = []
-				defaultProfile = @schedule.to_ScheduleRuleset.get.defaultDaySchedule
+				if cloned_clg_tstat_schedule.to_ScheduleRuleset.is_initialized
+					schedule = cloned_clg_tstat_schedule.to_ScheduleRuleset.get
+					# gather profiles of cloned schedule
+					profiles = []
+					cooling_thermostat_array = []
+					defaultProfile = schedule.to_ScheduleRuleset.get.defaultDaySchedule
 				
-				@profiles_h << defaultProfile
-				rules_h = @schedule.scheduleRules
-				rules_h.each do |rule_h|
-					@profiles_h << rule_h.daySchedule
-				end # end the rule_h do
+					profiles << defaultProfile
+					rules = schedule.scheduleRules
+				
+					rules.each do |rule|
+						profiles << rule.daySchedule
+					end # end the do loop through the rulesetsdo
 
-				#adjust profiles_h of temperature schedule of cloned schedule by + 1.5 deg F delta (0.833 C)
-				@profiles_h.each do |profile_h|
-					time_h = profile_h.times
-					i = 0
-					#TODO - name new profile
-					profile_h.values.each do |value_h|
-						delta_h = 0.8333
-						new_value_h = value_h - delta_h
+					#adjust profiles of temperature schedule of cloned schedule by + 1.5 deg F delta (0.833 C)
+					profiles.each do |profile|
+						time = profile.times
+						i = 0
+						#TODO - name new profile
+						profile.values.each do |value|
+							delta = 0.8333
+							new_value = value + delta # Note this is where the cooling setpoint is raised
+							profile.addValue(time[i], new_value)
+							i += 1
+							cloned_clg_tstat_schedule.setName(@new_clg_tstat_schedule_name)
+							cooling_thermostat_array << cloned_clg_tstat_schedule
+						end # end loop through each profile values
+					end # end loop through each profile
 					
-						profile_h.addValue(time_h[i], new_value_h)
-						i += 1
-						@cloned_htg_tstat_schedule.setName("#{@htg_tstat_schedule.name}-1.5F")
-						runner.registerInfo("The existing heating thermostat '#{@htg_tstat_schedule.name}' has been changed to '#{@htg_tstat_schedule.name}-1.5F'.")
-
-					end # end loop through each profile values
-
-				end # end loop through each profile_h
-
-			end # end if statement for changing schedule			
+				runner.registerInfo("The existing cooling thermostat '#{clg_tstat_schedule.name}' has been changed to #{cloned_clg_tstat_schedule.name}. Inspect the new schedule values using the OS App.")
+				end # end if statement for cloning and modifying cooling tstat schedule object
+				
+			end # end if statement for cooling Setpoint Temperature is initialized
 			
-		else
-			runner.registerInfo("The dual setpoint thermostat object named #{@zone_thermostat.name} serving thermal zone #{thermal_zone.name} did not have a heating setpoint temperature schedule associated with it. The measure will not alter this thermostat object")
-		end	# end is tatement for zone_thermostat.heatingSetpointTemperatureSchedule.is_initialized
+			if zone_thermostat.heatingSetpointTemperatureSchedule.is_initialized
+				htg_tstat_schedule = zone_thermostat.heatingSetpointTemperatureSchedule.get
+						
+				# clone the existing heating T-stat schedule in case it is used somewhere else
+				cloned_htg_tstat_schedule = htg_tstat_schedule.clone
+				
+				#name cloned heating t-stat schedule
+				cloned_htg_tstat_schedule.setName("#{htg_tstat_schedule.name}-1.5F")
 
-	end # end loop through thermal zone
-	
-	# ASHRAE 55 Comfort Warnings are applied to people objects
+				if cloned_htg_tstat_schedule.to_ScheduleRuleset.is_initialized
+					schedule = cloned_htg_tstat_schedule.to_ScheduleRuleset.get
+				
+					# gather profiles of cloned schedule
+					profiles_h = []
+					defaultProfile = schedule.to_ScheduleRuleset.get.defaultDaySchedule
+					
+					profiles_h << defaultProfile
+					rules_h = schedule.scheduleRules
+					rules_h.each do |rule_h|
+						profiles_h << rule_h.daySchedule
+					end # end the rule_h do
+
+					#adjust profiles_h of temperature schedule of cloned schedule by + 1.5 deg F delta (0.833 C)
+					profiles_h.each do |profile_h|
+						time_h = profile_h.times
+						i = 0
+						#TODO - name new profile
+						profile_h.values.each do |value_h|
+							delta_h = 0.8333
+							new_value_h = value_h - delta_h # Note this is where the heating setpoint is lowered 
+							profile_h.addValue(time_h[i], new_value_h)
+							i += 1
+						end # end loop through each profile values
+					end # end loop through each profile_h
+					
+				runner.registerInfo("The existing heating thermostat '#{htg_tstat_schedule.name}' has been changed to #{cloned_htg_tstat_schedule.name}. Inspect the new schedule values using the OS App.")
+				end # end if statement for cloning and modifying heating tstat schedule object	
+				
+			end # end if statement for heating Setpoint Temperature is initialized
+
+		else
+			runner.registerInfo("The dual setpoint thermostat object named #{@zone_thermostat.name} serving thermal zone #{thermal_zone.name} did not have a cooling setpoint temperature schedule associated with it. The measure will not alter this thermostat object")
+		end	# end if statement for zone_thermstat cooling schedule
+		
+	end # end loop through thermal zones			
+			
+	# Add ASHRAE 55 Comfort Warnings are applied to people objects
 	# get people objects and people definitions in model
-    people_defs = model.getPeopleDefinitions
-    people_instances = model.getPeoples
+	people_defs = model.getPeopleDefinitions
+	people_instances = model.getPeoples
 
-    # loop through people objects
-    people_def_counter = 0
-    people_defs.sort.each do |people_def|
-      next if not people_def.instances.size > 0
-      people_def_counter += 1
-      runner.registerInfo("Editing '#{people_def.name}'")
-      people_def.setEnableASHRAE55ComfortWarnings(true)
-    end
+	# loop through people objects
+	people_def_counter = 0
+	people_defs.sort.each do |people_def|
+	  next if not people_def.instances.size > 0
+	  people_def_counter += 1
+	  people_def.setEnableASHRAE55ComfortWarnings(true)
+	end
 			
 	reporting_frequency = "Timestep"
 	outputVariable = OpenStudio::Model::OutputVariable.new("Zone Thermal Comfort ASHRAE 55 Adaptive Model 90% Acceptability Status []",model)
-    outputVariable.setReportingFrequency(reporting_frequency)
-    runner.registerInfo("Adding output variable for 'Zone Thermal Comfort ASHRAE 55 Adaptive Model 90% Acceptability Status' reporting '#{reporting_frequency}'")
-	
+	outputVariable.setReportingFrequency(reporting_frequency)
+	runner.registerInfo("Adding output variable for 'Zone Thermal Comfort ASHRAE 55 Adaptive Model 90% Acceptability Status' reporting '#{reporting_frequency}'")
+
 	# write As Not Applicable message
 	if tz_count == 0
 		runner.registerAsNotApplicable("Measure is not applicable. There are no conditioned thermal zones in the model.")
 		return true
 	end
-	
-    # report initial condition of model
-    runner.registerInitialCondition("The initial model contained #{tz_count} thermal zones with #{@thermal_zone_array.length} 'Cooling Thermostat Schedule' and #{@thermal_zone_array.length} 'Heating Thermostat Schedule' objects for which this measure is applicable.")
 
-    # report final condition of model
-    runner.registerFinalCondition("The #{@thermal_zone_array.length} Heating and #{@thermal_zone_array.length} Cooling Thermostats schedules for #{@thermal_zone_array.length} Thermal Zones were altered to reflect a additional deadband width of 3 Deg F . ")
-    return true
+	# report initial condition of model
+	runner.registerInitialCondition("The initial model contained #{tz_count} thermal zones with #{thermal_zone_array.length} 'Cooling Thermostat Schedule' and #{thermal_zone_array.length} 'Heating Thermostat Schedule' objects for which this measure is applicable.")
+
+	# report final condition of model
+	runner.registerFinalCondition("The #{thermal_zone_array.length} Heating and #{thermal_zone_array.length} Cooling Thermostats schedules for #{thermal_zone_array.length} Thermal Zones were altered to reflect a additional deadband width of 3 Deg F . ")
+	return true
 
   end # end run method
   
@@ -184,33 +173,3 @@ end # end class
 
 # register the measure to be used by the application
 WidenThermostatSetpoint.new.registerWithApplication
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
